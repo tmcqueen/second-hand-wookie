84e6f19b140e5d577357b772548e5c10eb0e232b
Tim McQueen <tim.mcqueen@gmail.com>
2016-03-01 12:19:13 -0600

    added index page

--- gulpfile.js
+++ gulpfile.js
@@ -14,17 +14,20 @@ var elixir = require('laravel-elixir');
 elixir(function(mix) {
     // mix.sass('app.scss');
     mix.styles([
-        'css/main.css',
         'libs/bootstrap/dist/css/bootstrap.css',
         'libs/bootstrap/dist/css/bootstrap-theme.css',
         'libs/font-awesome/css/font-awesome.css',
     ], 'public/css/app.css', 'resources/assets');
+
+    mix.styles(['main.css'], 'public/css/main.css', 'resources/assets/css');
 
     mix.scripts([
         'libs/jquery/dist/jquery.js',
         'libs/vue/dist/vue.js',
         'libs/bootstrap/dist/js/bootstrap.js',
     ], 'public/js/app.js', 'resources/assets');
+
+    mix.scripts(['main.js'], 'public/js/main.js', 'resources/assets/js');
 
     mix.copy('resources/assets/libs/bootstrap/dist/fonts', 'public/fonts');
     mix.copy('resources/assets/libs/font-awesome/fonts', 'public/fonts');
---
+++ resources/views/index.blade.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <meta name="robots" content="noindex, nofollow">
+    <meta name="googlebot" content="noindex, nofollow">
+
+    <link rel="stylesheet" type="text/css" href="/css/app.css">
+    <link rel="stylesheet" type="text/css" href="/css/main.css">
+
+
+    <title>Foomatic</title>
+
+</head>
+<body>
+    <nav id="first-nav" class="navbar navbar-fixed-top">
+    <div class="container">
+        <div class="navbar-header">
+        <div class="nav navbar-nav">
+            <a href="#" class="navbar-brand">
+            <img id="foomatic-logo" src="https://foomatic.org/wp-content/uploads/2015/11/Foo-PNG.png" alt="Foomatic">
+            </a>
+        </div>
+        </div>
+    </div>
+    </nav>
+
+
+    <nav id="second-nav" class="navbar navbar-fixed-top navbar-inverse">
+    <div class="container">
+        <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+        <ul class="nav navbar-nav">
+            <li id="home" class="active"><a href="#">Home</a></li>
+            <li id="about"><a href="#about">About</a></li>
+            <li id="contact"><a href="#contact">Contact</a></li>
+            <li id="events"><a href="#events">Events</a></li>
+        </ul>
+        </div>
+        <!--/.nav-collapse -->
+    </div>
+    </nav>
+
+    <div class="jumbotron">
+    <div class="container">
+        <h1>Call to Action!</h1>
+    </div>
+    </div>
+
+    <div class="container">
+
+    <div class="row">
+        <div class="col-md-4">
+        <h2>Our Mission</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque praesentium necessitatibus assumenda, ex quod. Fugiat culpa pariatur cupiditate, quam, voluptatibus nam veniam illo eius aspernatur repellendus quidem minus consectetur quasi!</p>
+        </div>
+        <div class="col-md-4">
+        <h2>What is a Makerspace?</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque praesentium necessitatibus assumenda, ex quod. Fugiat culpa pariatur cupiditate, quam, voluptatibus nam veniam illo eius aspernatur repellendus quidem minus consectetur quasi!</p>
+        </div>
+        <div class="col-md-4">
+        <h2>Get Involved</h2>
+        <a href="#" class="btn btn-lg btn-primary btn-block">Join</a>
+        <a href="#" class="btn btn-lg btn-success btn-block">Donate</a>
+        <a href="#" class="btn btn-lg btn-warning btn-block">Volunteer</a>
+        </div>
+    </div>
+    </div>
+
+    <div class="divider"></div>
+
+    <!-- /.container -->
+    <div class="container news">
+    <div class="row">
+        <div class="col-md-12">
+        <h2>Maker News</h2>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-4">
+        <img src="https://foomatic.org/wp-content/uploads/2015/11/1454994_10101807623295291_1499713025_n-e1447569959935.jpg" alt="" class="img-thumbnail">
+        </div>
+        <div class="col-md-8">
+        <h2>Maker of the year!</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates laudantium, nam laboriosam ipsa consequuntur esse repellendus, tempora inventore amet, eum dolorum distinctio aut beatae soluta alias iusto nobis perspiciatis. Hic!</p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-4">
+        <img src="https://foomatic.org/wp-content/uploads/2015/11/12243893_10103593744439405_563631143_n.jpg" alt="" class="img-thumbnail">
+        </div>
+        <div class="col-md-8">
+        <h2>Private eye is on the case for the missing tools!</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates laudantium, nam laboriosam ipsa consequuntur esse repellendus, tempora inventore amet, eum dolorum distinctio aut beatae soluta alias iusto nobis perspiciatis. Hic!</p>
+        </div>
+    </div>
+    </div>
+    <div class="divider"></div>
+    <footer class="footer">
+    <div class="container">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente atque veritatis nisi expedita, ducimus excepturi debitis, sequi officia quis? Labore ipsam molestias nam, sit placeat ut natus sunt porro facilis.</p>
+    </div>
+    </footer>
+
+    <script type="text/javascript" src="/js/app.js"></script>
+    <script type="text/javascript" src="/js/main.js"></script>
+</body>
+</html>
