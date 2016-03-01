a49e21ab849a17affeb1ecf26cde2b0f21899189
Tim McQueen <tim.mcqueen@gmail.com>
2016-03-01 09:15:59 -0600

    Configured Gulp & Laravel Elixir

--- gulpfile.js
+++ gulpfile.js
@@ -12,5 +12,20 @@ var elixir = require('laravel-elixir');
  */
 
 elixir(function(mix) {
-    mix.sass('app.scss');
+    // mix.sass('app.scss');
+    mix.styles([
+        'css/main.css',
+        'libs/bootstrap/dist/css/bootstrap.css',
+        'libs/bootstrap/dist/css/bootstrap-theme.css',
+        'libs/font-awesome/css/font-awesome.css',
+    ], 'public/css/app.css', 'resources/assets');
+
+    mix.scripts([
+        'libs/jquery/dist/jquery.js',
+        'libs/vue/dist/vue.js',
+        'libs/bootstrap/dist/js/bootstrap.js',
+    ], 'public/js/app.js', 'resources/assets');
+
+    mix.copy('resources/assets/libs/bootstrap/dist/fonts', 'public/fonts');
+    mix.copy('resources/assets/libs/font-awesome/fonts', 'public/fonts');
 });
